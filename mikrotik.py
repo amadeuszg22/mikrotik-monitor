756167b414ef77831d330e2394a48224a530da27
Amadeusz Grabowski <amadeusz.grabowski@gmail.com>
2021-01-19 19:54:42 +0100

    Added exporitng for eht1 tx,rx cpu and dhcp

--- mikrotik.py
+++ mikrotik.py
@@ -8,6 +8,8 @@ from librouteros import connect
 import logging
 from datetime import datetime
 import configparser
+from prometheus_client import start_http_server, Summary,Gauge
+import random
 
 time_now = datetime.now()
 gen_log = logging
@@ -20,9 +22,12 @@ class config:
 	host=conf.get('r01','host')
 	user=conf.get('r01','user')
 	password=conf.get('r01','password')
-    apikey=conf.get('r01','aio_key')
-
-	
+	apikey=conf.get('r01','aio_key')
+	tx = Gauge('if_ether_tx', 'eth1 tramsmitted data')
+	rx = Gauge('if_ether_rx', 'eth1 received data')
+	cpu=Gauge('mt_cpu', 'mikrotik cpu utilization')
+	dhcp_l=Gauge('mt_dhcp_lease', 'dhcp leases')
+	dhcp_l_a=Gauge('mt_dhcp_lease_active', 'dhcp leases')
 #result = api(cmd='/interface/print', stats=True)
 #for a in result:
 #	print (a)
@@ -35,7 +40,7 @@ def feed(value,mo):
 	if mo == "tx":
 		try:
 			url ="https://io.adafruit.com/api/v2/Ama_g/feeds/ether1-tx/data.json"
-			headers = {'X-AIO-Key': 'apikey','Content-Type': 'application/json'}
+			headers = {'X-AIO-Key': config.apikey,'Content-Type': 'application/json'}
 			data = '{"value": "'+str(value)+'"}'
 			r = requests.post(url, data=data, headers=headers)
 			results = r.json()
@@ -45,7 +50,7 @@ def feed(value,mo):
 	if mo == "rx":
 		try:
 			url ="https://io.adafruit.com/api/v2/Ama_g/feeds/ether1-rx/data.json"
-			headers = {'X-AIO-Key': 'apikey','Content-Type': 'application/json'}
+			headers = {'X-AIO-Key': config.apikey,'Content-Type': 'application/json'}
 			data = '{"value": "'+str(value)+'"}'
 			r = requests.post(url, data=data, headers=headers)
 			results = r.json()
@@ -55,7 +60,7 @@ def feed(value,mo):
 	if mo == "cpu":
 		try:
 			url ="https://io.adafruit.com/api/v2/Ama_g/feeds/r01-cpu/data.json"
-			headers = {'X-AIO-Key': 'apikey','Content-Type': 'application/json'}
+			headers = {'X-AIO-Key': config.apikey,'Content-Type': 'application/json'}
 			data = '{"value": "'+str(value)+'"}'
 			r = requests.post(url, data=data, headers=headers)
 			results = r.json()
@@ -77,18 +82,15 @@ def main():
 				r=round((float(a.get("rx-bits-per-second")/1024)/1024),3)
 				feed(t,"rx")
 				feed(r,"tx")
-				#i2c_lcd.lcd_string("Traffic Monitor",i2c_lcd.LCD_LINE_1)
-				#i2c_lcd.lcd_string("................",i2c_lcd.LCD_LINE_2)
-				#time.sleep(1)
-				#i2c_lcd.clear()
-				#i2c_lcd.lcd_string("Download:"+str(r)+"Mbps",i2c_lcd.LCD_LINE_1)
-				#i2c_lcd.lcd_string("Upload:"+str(t)+"Mbps",i2c_lcd.LCD_LINE_2)
-				#time.sleep(60)
+				config.tx.set(t)
+				config.rx.set(r)
+
 			params = {'cpu-load'}
 			result = api(cmd='/system/resource/print')
 			for a in result:
 				print ("CPU Load:",a.get("cpu-load"),"%")
 				feed(a.get("cpu-load"),"cpu")
+				config.cpu.set(a.get("cpu-load"))
 			
 			dhcp_lease =api(cmd='/ip/dhcp-server/lease/print')
 			dchp_act_lease = 0
@@ -98,7 +100,8 @@ def main():
 					dchp_act_lease = dchp_act_lease +1
 			print ("dhcp_lease",len(dhcp_lease))
 			print ("dhcp_active_lease",dchp_act_lease)
-
+			config.dhcp_l.set(dhcp_lease)
+			config.dhcp_l_a.set(dchp_act_lease)
 
 			caps_reg =api(cmd='/caps-man/registration-table/print')
 			print ("capsman_reg-table",len(caps_reg))
@@ -138,12 +141,13 @@ def main():
 
 			logging.warning('time={0} ether1tx={1} ether1rx={2} cpu={3} dhcp_lease={4} caps-client={5} dchp_act_lease={6} fw_conn_val={7} fw_conn_tcp={8} fw_conn_udp={9} fw_conn_gre={10} fw_conn_icmp={11} fw_conn_igmp={12}'.format(time_now,t,r,a.get("cpu-load"),len(dhcp_lease),len(caps_reg),dchp_act_lease,len(fw_conn),fw_conn_tcp,fw_conn_udp,fw_conn_gre,fw_conn_icmp,fw_conn_igmp))
 			#wireless_log.warning('time={0} caps-client={1}'.format(time_now,len(caps_reg)))
-			time.sleep(60)
+			time.sleep(30)
 
 
-
+		
 		except (librouteros.exceptions.ConnectionError):
 				print ('Timeout')
 
 if __name__=="__main__":
-        main()
+	start_http_server(8000)
+	main()
